c33def50888515b9fdf7572b0b27cbf4743f65e2
wayne james <jackmcwilliams@Jacks-Air.lan>
2022-10-23 21:38:55 -0700

    built employee cards

--- layout.js
+++ layout.js
@@ -12,10 +12,44 @@ const teamCards = cards => {
             <br>
             <p>Email: <a href = "mailto:${mEmail}">${mEmail}</a></p>
             <br>
-            <p> Office:${mOffice}</p>
+            <p> Office Number:${mOffice}</p>
         </div>
-        
-        
+    </section>
+`
+    }
+
+    const eCard = engineer => {
+        return 
+     `<section class="card engineer-card">
+        <div class="card-top bg-primary text-light">
+        <h5>${eName}</h5>
+        <h6>&#xe00c Engineer</h6>
+        </div>
+        <div class="card-body">
+            <p>ID:${eID}</p>
+            <br>
+            <p>Email: <a href = "mailto:${eEmail}">${eEmail}</a></p>
+            <br>
+            <p>Github: <a href="https://github.com/${github}">${github}</p>
+        </div>
+    </section>
+`
+    }
+
+    const iCard = intern => {
+        return 
+     `<section class="card engineer-card">
+        <div class="card-top bg-primary text-light">
+        <h5>${iName}</h5>
+        <h6>&#xe51a Intern</h6>
+        </div>
+        <div class="card-body">
+            <p>ID:${iID}</p>
+            <br>
+            <p>Email: <a href = "mailto:${iEmail}">${iEmail}</a></p>
+            <br>
+            <p>School: ${school}</p>
+        </div>
     </section>
 `
     }
